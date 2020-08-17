101de03a63714f41d19982aba473382bec126225
abdielrumaldom <abdielrumaldom@gmail.com>
2020-08-16 22:57:11 -0500

    Added gate confirm functionality with source

--- phoneCall.py
+++ phoneCall.py
@@ -2,46 +2,26 @@ from flask import Flask, request
 from twilio.twiml.voice_response import Gather, VoiceResponse, Say, Play
 from twilio.rest import Client
 from decouple import config
+import ast
 
 """
 Requires Flask, decouple and Twillio 
 """
 app = Flask(__name__)
 
-KEY = False
-
-
-def send_message():
-    """
-    Sends a text to all the configured tenants that someone is at the door.
-    """
-    # Configure SMS Agent
-    account_sid = config('ACCOUNT_SID')
-    auth_token = config('AUTH_TOKEN')
-    client = Client(account_sid, auth_token)
-
-    # Configure Tenants
-    tenants = [config('ABE_NUM'), config('BRANI_NUM'), config('MARCEL_NUM')]
-
-    # Send a message to each of the tenants
-    for user in tenants:
-        message = client.messages.create(
-            body='Someone is at the gate. Let them in?',
-            from_=config('BOT_NUM'),
-            to=user
-        )
-
-    return str(message)
-
+TENANT_KEY = False
 
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
     """
     Picks up phone, initiates prompt and send text to tenant
     """
+    # Set global key for manipulation
+    global TENANT_KEY
 
-    # message the tenants
-    send_message()
+    # Reset key value and message the tenants that someone is at the gate
+    TENANT_KEY = False
+    send_message('Someone is at the gate. Let them in?')
     print("Message sent to tenants")
 
     # Start Prompt
@@ -69,8 +49,6 @@ def voice():
 
 @app.route("/verify", methods=['GET', 'POST'])
 def verify():
-    # Set global key for manipulation
-    global KEY
 
     print("verification started")
 
@@ -78,15 +56,10 @@ def verify():
     answer = VoiceResponse()
 
     # We want the tenants to have priority over who is allowed and who is not
-    if KEY:
+    if TENANT_KEY:
         answer.play('', digits='9ww9ww9')
-
-        # TODO: Add function that tells the tenants that the gate has been opened.
-
-        # after the gate is open, reset KEY
-        KEY = False
-    else:
-        answer.play("https://www.myinstants.com/media/sounds/gandalf_shallnotpass.mp3")
+        # tells the tenants that the gate has been opened.
+        send_message('The gate that been opened by a gatekeeper.')
 
     #  Process Digits
     if 'Digits' in request.values:
@@ -95,14 +68,12 @@ def verify():
 
         if choice == config('GATE_CODE'):
             answer.play('', digits='9ww9ww9')
-
-            # TODO: Add function that tells the tenants that the gate has been opened.
-
-            # after the gate is open, reset KEY
-            KEY = False
+            # tells the tenants that the gate has been opened.
+            send_message('The gate that been opened by the correct code.')
             return str(answer)
 
-    KEY = False
+    # If they were not let in, play "You shall not pass! :)"
+    answer.play("https://www.myinstants.com/media/sounds/gandalf_shallnotpass.mp3")
     return str(answer)
 
 
@@ -112,20 +83,51 @@ def incoming_sms():
     Get a reply from the tenants
     """
     # Set global key for manipulation
-    global KEY
+    global TENANT_KEY
+    tenants = ast.literal_eval(config("TENANTS"))
 
-    # Get the message the user sent our Twilio number
+    # Get the message and number the user sent our Twilio number
     body = request.values.get('Body', None)
+    number = request.values.get('From', None)
 
     # We noticed that some devices added more than just 'yes'
     # maybe it's extra padding. As long as there is a yes we should
     # open the gate
     if 'yes' in body.lower():
-        KEY = True
+        TENANT_KEY = True
     if 'no' in body.lower():
-        KEY = False
+        TENANT_KEY = False
+
+    # Tell the other tenants who replied with what
+    for key, value in tenants.items():
+        if value == number:
+            message = "{} has replied with {}".format(key, body)
+            send_message(message)
 
     return 'placeholder'
+
+
+def send_message(prompt):
+    """
+    Sends a text to all the configured tenants.
+    """
+    # Configure SMS Agent
+    account_sid = config('ACCOUNT_SID')
+    auth_token = config('AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+
+    # retrieve tenants
+    tenants = ast.literal_eval(config("TENANTS"))
+
+    # Send a message to each of the tenants
+    for number in tenants.values():
+        message = client.messages.create(
+            body=prompt,
+            from_=config('BOT_NUM'),
+            to=number
+        )
+
+    return str(message)
 
 
 if __name__ == "__main__":
